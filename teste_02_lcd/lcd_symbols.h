89b51c9b6c416dc3efe153b743aaad68614be0bc
backtrunck <backtrunck@yahoo.com.br>
2017-10-12 23:07:59 -0300

    identificação do pinos do controlador para LCD

--- teste_02_lcd/lcd_symbols.h
+++ teste_02_lcd/lcd_symbols.h
@@ -8,3 +8,56 @@
             .define     0xFC,   DIGITO_7
             .define     0xFC,   DIGITO_8
             .define     0xFC,   DIGITO_9
+
+
+            ;Para msp430fr6989
+            ;P1.4->S1 Pino 1
+            ;P1.5->S0 Pino 3
+            ;P6.4->S36 Pino 11
+            ;P6.3               COM0
+            ;P6.2               COM1
+            ;P6.5               COM2
+            ;P6.6               COM3
+            ;P2.4->S43 Pino 14  COM4
+            ;P2.5->S42 Pino 15  COM5
+            ;P2.6->S41 Pino 16  COM6
+            ;P2.7->S40 Pino 17  COM7
+            ;P10.2->S39 Pino 18
+            ;P5.0->S43 Pino 19
+            ;P5.1->S37 Pino 20
+            ;P5.2->S36 Pino 22
+            ;P5.3->S35 Pino 22
+            ;P3.0->S34 Pino 23
+            ;P3.1->S33 Pino 24
+            ;P3.2->S32 Pino 25
+            ;P6.7->S31 Pino 34
+            ;P7.5->S30 Pino 35
+            ;P7.6->S29 Pino 36
+            ;P10.1->S28 Pino 37
+            ;P7.7->S27 Pino 38
+            ;P3.3->S26 Pino 39
+            ;P3.4->S25 Pino 40
+            ;P3.5->S24 Pino 41
+            ;P3.6->S23 Pino 42
+            ;P3.7->S22 Pino 43
+            ;P8.0->S21 Pino 44
+            ;P8.1->S20 Pino 45
+            ;P8.2->S19 Pino 46
+            ;P8.3->S18 Pino 47
+            ;P7.0->S17 Pino 52
+            ;P7.1->S16 Pino 53
+            ;P7.2->S15 Pino 54
+            ;P7.3->S14 Pino 55
+            ;P7.4->S13 Pino 56
+            ;P5.4->S12 Pino 87
+            ;P5.5->S11 Pino 88
+            ;P5.6->S10 Pino 89
+            ;P5.7->S9 Pino 90
+            ;P4.4->S8 Pino 91
+            ;P4.5->S7 Pino 92
+            ;P4.6->S6 Pino 93
+            ;P4.7->S5 Pino 94
+            ;P10.0->S4 Pino 95
+            ;P4.0->S3 Pino 96
+            ;P4.1->S2 Pino 97
+
