fd3e232a876f06c3799d82a0b70e75609e942115
backtrunck <backtrunck@yahoo.com.br>
2017-11-01 17:34:05 -0300

    inclusão do teste em C da Uart

--- echo_uart_01/main.c
+++ echo_uart_01/main.c
@@ -60,10 +60,10 @@
 //
 //                MSP430FR6989
 //             -----------------
-//       RST -|     P2.0/UCA0TXD|----> PC (echo)
+//       RST -|     P2.0/UCA1TXD|----> PC (echo)
 //            |                 |
 //            |                 |
-//            |     P2.1/UCA0RXD|<---- PC
+//            |     P2.1/UCA1RXD|<---- PC
 //            |                 |
 //
 //   William Goh
@@ -78,8 +78,11 @@ int main(void)
   WDTCTL = WDTPW | WDTHOLD;                 // Stop Watchdog
 
   // Configure GPIO
-  P2SEL0 |= BIT0 | BIT1;                    // USCI_A0 UART operation
-  P2SEL1 &= ~(BIT0 | BIT1);
+  //P2SEL0 |= BIT0 | BIT1;                    // USCI_A0 UART operation
+  //P2SEL1 &= ~(BIT0 | BIT1);
+
+  P3SEL0 = BIT4  | BIT5;
+  P3SEL1 = ~(BIT4 | BIT5);
 
   // Disable the GPIO power-on default high-impedance mode to activate
   // previously configured port settings
@@ -93,43 +96,51 @@ int main(void)
   CSCTL0_H = 0;                             // Lock CS registers
 
   // Configure USCI_A0 for UART mode
-  UCA0CTLW0 = UCSWRST;                      // Put eUSCI in reset
-  UCA0CTLW0 |= UCSSEL__SMCLK;               // CLK = SMCLK
+  UCA1CTLW0 = UCSWRST;                      // Put eUSCI in reset
+  UCA1CTLW0 |= UCSSEL__SMCLK;               // CLK = SMCLK
   // Baud Rate calculation
   // 8000000/(16*9600) = 52.083
   // Fractional portion = 0.083
   // User's Guide Table 21-4: UCBRSx = 0x04
   // UCBRFx = int ( (52.083-52)*16) = 1
-  UCA0BR0 = 52;                             // 8000000/16/9600
-  UCA0BR1 = 0x00;
-  UCA0MCTLW |= UCOS16 | UCBRF_1 | 0x4900;
-  UCA0CTLW0 &= ~UCSWRST;                    // Initialize eUSCI
-  UCA0IE |= UCRXIE;                         // Enable USCI_A0 RX interrupt
-  UCA0TXBUF = 'a';
-  __bis_SR_register(LPM3_bits | GIE);       // Enter LPM3, interrupts enabled
+  UCA1BR0 = 52;                             // 8000000/16/9600
+  UCA1BR1 = 0x00;
+  UCA1MCTLW |= UCOS16 | UCBRF_1 | 0x4900;
+  UCA1CTLW0 &= ~UCSWRST;                    // Initialize eUSCI
+  UCA1IE |= UCRXIE;                         // Enable USCI_A0 RX interrupt
+  UCA1IE |= UCTXIE;
+
+  //__bis_SR_register(LPM3_bits | GIE);       // Enter LPM3, interrupts enabled
+  __bis_SR_register( GIE);       // Enter LPM3, interrupts enabled
+
   __no_operation();                         // For debugger
+
+  while(1);
+
 }
 
 #if defined(__TI_COMPILER_VERSION__) || defined(__IAR_SYSTEMS_ICC__)
-#pragma vector=USCI_A0_VECTOR
+#pragma vector=USCI_A1_VECTOR
 __interrupt void USCI_A0_ISR(void)
 #elif defined(__GNUC__)
-void __attribute__ ((interrupt(USCI_A0_VECTOR))) USCI_A0_ISR (void)
+void __attribute__ ((interrupt(USCI_A1_VECTOR))) USCI_A1_ISR (void)
 #else
 #error Compiler not supported!
 #endif
 {
-  switch(__even_in_range(UCA0IV, USCI_UART_UCTXCPTIFG))
+  switch(__even_in_range(UCA1IV, USCI_UART_UCTXCPTIFG))
   {
     case USCI_NONE: break;
     case USCI_UART_UCRXIFG:
-      while(!(UCA0IFG&UCTXIFG));
-      UCA0TXBUF = UCA0RXBUF;
-      __no_operation();
-      break;
+      //while(!(UCA1IFG & UCTXIFG))
+          UCA1TXBUF = UCA1RXBUF;
+          __no_operation();
+          break;
     case USCI_UART_UCTXIFG: break;
     case USCI_UART_UCSTTIFG: break;
     case USCI_UART_UCTXCPTIFG: break;
   }
 }
+
+
 
